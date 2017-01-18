95fc60008dc870abd84504e5f2c500a8d9757a60
Ivan Pogrebnyak <ivan.pogrebnyak@gmail.com>
2017-01-18 01:55:36 -0500

    added tbrowser

---
+++ src/tbrowser.cc
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <vector>
+#include <memory>
+
+#include <TApplication.h>
+#include <TBrowser.h>
+#include <TFile.h>
+
+int main(int argc, char* argv[])
+{
+  int app_argc = 1;
+  char app_argv1[] = "l";
+  char *app_argv[] = { app_argv1 };
+  TApplication app("App",&app_argc,app_argv);
+
+  std::vector<std::unique_ptr<TFile>> files;
+  files.reserve(argc-1);
+  // std::cout << std::endl;
+  for (int i=1; i<argc; ++i) {
+    files.emplace_back(new TFile(argv[i]));
+    // std::cout << files.back()->GetName();
+  }
+  // std::cout << std::endl;
+
+  TBrowser b;
+
+  app.Run();
+  return 0;
+}
