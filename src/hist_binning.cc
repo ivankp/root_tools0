3c7112ceba85b7a483b7307b5d9f3e72e92188b8
ivankp <ivan.pogrebnyak@gmail.com>
2017-07-14 16:20:32 -0400

    hist_binning: fold uniform

--- src/hist_binning.cc
+++ src/hist_binning.cc
@@ -3,7 +3,6 @@
 #include <iostream>
 #include <vector>
 #include <unordered_map>
-#include <memory>
 #include <algorithm>
 
 #include <TFile.h>
@@ -20,15 +19,32 @@ using std::cerr;
 using std::endl;
 
 int main(int argc, char* argv[]) {
-  const auto f = std::make_unique<TFile>(argv[1]);
-  if (f->IsZombie()) return 1;
+  if (argc!=3) {
+    cout << "usage: " << argv[0] << " file.root dir/hist" << endl;
+    return 1;
+  }
 
-  TH1 *h = dynamic_cast<TH1*>(f->Get(argv[2]));
+  TFile f(argv[1]);
+  if (f.IsZombie()) return 1;
+
+  TH1 *h = dynamic_cast<TH1*>(f.Get(argv[2]));
   TAxis *xa = h->GetXaxis();
   if (xa->IsVariableBinSize()) {
     const TArrayD& bins = *xa->GetXbins();
-    for (Int_t i=0, n=bins.GetSize(); i<n; ++i)
-      cout << bins[i] << '\n';
+    const auto n = bins.GetSize();
+    const double diff = bins[1] - bins[0];
+    bool uniform = true;
+    for (Int_t i=1; i<n; ++i) {
+      if (std::abs(1.-(bins[i]-bins[i-1])/diff) > 1e-8) {
+        uniform = false;
+        break;
+      }
+    }
+    if (uniform)
+      cout << (n-1) << ": " << bins[0] << ' ' << bins[n-1] << '\n';
+    else
+      for (Int_t i=0; i<n; ++i)
+        cout << bins[i] << '\n';
     cout.flush();
   } else {
     cout << xa->GetNbins() << ": "
